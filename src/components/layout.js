7419297d400024743a7a090fc6e5ec152f9c8161
kwroburr <robertsonwkent@gmail.com>
2021-11-28 20:21:59 -0500

    added styling to nav menu in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,13 @@
 import * as React from "react"
 import { Link } from 'gatsby'
-import { container, nav, navMenu, navLinks, navLinkso, heading, button} from "./layout.module.css"
+import { container, nav, navMenu, navLinks, span, heading, button} from "./layout.module.css"
 
 const Layout = ({ pageTitle,  pageHeading, children }) => {
   return(
     <main className = {container}>
     <title>{pageTitle}</title>
       <nav className={nav}>
-        <Link to="/" className={navLinks} >Bakery Boys</Link>
+        <Link to="/" className={navLinks}><span className={span}>Bakery Boys</span></Link>
         <ul className={navMenu}>
         <li><Link to="/menu" className={navLinks}>Menu</Link></li>
         <li><Link to="/products" className={navLinks}>Products</Link></li>
