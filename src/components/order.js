dabf0eb9a620d4c0aedaa3dc87b4d6b5a7f2faa0
kwroburr <robertsonwkent@gmail.com>
2021-12-04 10:05:59 -0500

    changed order div positioning

--- src/components/order.js
+++ src/components/order.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from 'gatsby'
-import {container, button, buttonCon, info, line } from "./order.module.css"
+import {container, button, buttonCon, info, line, words } from "./order.module.css"
 
 const OrderDiv = () =>{
   const hLine = ({ color }) => (
@@ -15,15 +15,17 @@ const OrderDiv = () =>{
 
   return(
     <main className={container}>
-      <div className={buttonCon}>
+      <div className={info}>
         <button className={button}>Order Now</button>
       </div>
       <div className={info}>
+        <div className={words}>
         <h3>HOURS - MON-FRI 7AM - 5PM, SAT 7AM-4PM, CLOSED SUN.</h3>
         <h3>*BREAKFAST SERVED FROM 7AM-11AM</h3>
         <h3>*LUNCH SERVED FROM 11AM-3PM</h3>
         <h3>PICK-UP IN STORE OR DELIVERY!</h3>
         <h3>WE STILL OFFER CURBSIDE AT ARRIVAL (423) 756-0303</h3>
+      </div>
       </div>
       <div>
         <hr></hr>
