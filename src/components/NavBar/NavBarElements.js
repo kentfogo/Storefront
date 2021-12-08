423a264e641ac52fc5181eb6c0ebb918b1b1319f
kwroburr <robertsonwkent@gmail.com>
2021-12-08 14:03:50 -0500

    changed background of nav in media query

--- src/components/NavBar/NavBarElements.js
+++ src/components/NavBar/NavBarElements.js
@@ -26,8 +26,9 @@ background: ${({ active }) =>
   box-shadow: inset 0 0 0 1000px rgba(0,0,0,0.3);
 
   @media screen and (max-width: 960px) {
-    background: ${({ click }) => (click ? "#fff" : "transparent")};
+    /*background: ${({ click }) => (click ? "#fff" : "transparent")};*/
     transition: 0.8s all ease;
+    height: auto;
   }
 `
 
