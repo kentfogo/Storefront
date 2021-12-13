efd392b66568643920f57870d29731feaa4a5a42
kwroburr <robertsonwkent@gmail.com>
2021-12-13 17:48:31 -0500

    added footer to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,14 @@ import * as React from "react";
 import { Link } from 'gatsby'
 import Navbar from '../components/about/index.js'
 import AboutPage from "../components/About/content"
+import Footer from "../components/footer"
 
 const About = () => {
   return (
     <main>
     <Navbar />
     <AboutPage />
+    <Footer />
     </main>
   );
 };
