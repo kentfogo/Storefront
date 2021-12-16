e1e300d86aacf887151ecdb04d2b7c6e5c561387
kwroburr <robertsonwkent@gmail.com>
2021-12-16 18:18:21 -0500

    fixing the menu banner

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,17 +1,20 @@
 import * as React from "react"
 import { Link } from 'gatsby'
-import MenuLayout from '../components/MenuPage'
+import MenuDiv from '../components/Menu/MenuDiv'
 import Footer from '../components/footer'
 import Layout from '../components/layout/layout'
 import { menu } from "./menu.module.css"
 
 const Menu = () => {
   return (
-    <main className = {menu}>
+    <>
+      <div className = {menu}>
+        <Layout pageTitle='Menu'/>
+      </div>
+      <MenuDiv />
+      <Footer />
 
-      <Layout />
-       <Footer />
-    </main>
+    </>
   )
 }
 
