d8c2b81903bc44926f5e5306c3ed3737dd2be5ca
kwroburr <robertsonwkent@gmail.com>
2021-12-21 14:07:37 -0500

    made order div responsive

--- src/components/Order/order.js
+++ src/components/Order/order.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from 'gatsby'
-import {container, button, buttonCon, info, line, words } from "./order.module.css"
+import {container, button, info, line, words, wrapper } from "./order.module.scss"
 
 const OrderDiv = () =>{
   const hLine = ({ color }) => (
@@ -14,24 +14,23 @@ const OrderDiv = () =>{
   );
 
   return(
-    <main className={container}>
-      <div className={info}>
-        <button className={button}>Order Now</button>
-      </div>
+    <main className={wrapper}>
+      <section className={container}>
         <div className={info}>
-          <div className={words}>
-            <h3>HOURS - MON-FRI 7AM - 5PM, SAT 7AM-4PM, CLOSED SUN.</h3>
-            <h3>*BREAKFAST SERVED FROM 7AM-11AM</h3>
-            <h3>*LUNCH SERVED FROM 11AM-3PM</h3>
-            <h3>PICK-UP IN STORE OR DELIVERY!</h3>
-            <h3>WE STILL OFFER CURBSIDE AT ARRIVAL (423) 756-0303</h3>
-            <div className={line}></div>
-          </div>
+          <button className={button}>Order Now</button>
         </div>
-
-
+        <div className={info}>
+          <h3>HOURS - MON-FRI 7AM - 5PM, SAT 7AM-4PM, CLOSED SUN.</h3>
+          <h3>*BREAKFAST SERVED FROM 7AM-11AM</h3>
+          <h3>*LUNCH SERVED FROM 11AM-3PM</h3>
+          <h3>PICK-UP IN STORE OR DELIVERY!</h3>
+          <h3>WE STILL OFFER CURBSIDE AT ARRIVAL (423) 756-0303</h3>
+        </div>
+      </section>
 
     </main>
+
+
 
 
 
